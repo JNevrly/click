a24f35a25072947c6779dfafef54e7b1670faec5
Josef <jnevrly@alps.cz>
2019-05-03 10:25:14 +0200

    Removed check for UTF-8 locales

--- click/_unicodefun.py
+++ click/_unicodefun.py
@@ -59,67 +59,69 @@ def _verify_python3_env():
     if fs_enc != 'ascii':
         return
 
-    extra = ''
-    if os.name == 'posix':
-        import subprocess
-        try:
-            rv = subprocess.Popen(['locale', '-a'], stdout=subprocess.PIPE,
-                                  stderr=subprocess.PIPE).communicate()[0]
-        except OSError:
-            rv = b''
-        good_locales = set()
-        has_c_utf8 = False
+    return  # Basically ignore the utf-8 locale checking
 
-        # Make sure we're operating on text here.
-        if isinstance(rv, bytes):
-            rv = rv.decode('ascii', 'replace')
-
-        for line in rv.splitlines():
-            locale = line.strip()
-            if locale.lower().endswith(('.utf-8', '.utf8')):
-                good_locales.add(locale)
-                if locale.lower() in ('c.utf8', 'c.utf-8'):
-                    has_c_utf8 = True
-
-        extra += '\n\n'
-        if not good_locales:
-            extra += (
-                'Additional information: on this system no suitable UTF-8\n'
-                'locales were discovered.  This most likely requires resolving\n'
-                'by reconfiguring the locale system.'
-            )
-        elif has_c_utf8:
-            extra += (
-                'This system supports the C.UTF-8 locale which is recommended.\n'
-                'You might be able to resolve your issue by exporting the\n'
-                'following environment variables:\n\n'
-                '    export LC_ALL=C.UTF-8\n'
-                '    export LANG=C.UTF-8'
-            )
-        else:
-            extra += (
-                'This system lists a couple of UTF-8 supporting locales that\n'
-                'you can pick from.  The following suitable locales were\n'
-                'discovered: %s'
-            ) % ', '.join(sorted(good_locales))
-
-        bad_locale = None
-        for locale in os.environ.get('LC_ALL'), os.environ.get('LANG'):
-            if locale and locale.lower().endswith(('.utf-8', '.utf8')):
-                bad_locale = locale
-            if locale is not None:
-                break
-        if bad_locale is not None:
-            extra += (
-                '\n\nClick discovered that you exported a UTF-8 locale\n'
-                'but the locale system could not pick up from it because\n'
-                'it does not exist.  The exported locale is "%s" but it\n'
-                'is not supported'
-            ) % bad_locale
-
-    raise RuntimeError(
-        'Click will abort further execution because Python 3 was'
-        ' configured to use ASCII as encoding for the environment.'
-        ' Consult https://click.palletsprojects.com/en/7.x/python3/ for'
-        ' mitigation steps.' + extra
-    )
+    # extra = ''
+    # if os.name == 'posix':
+    #     import subprocess
+    #     try:
+    #         rv = subprocess.Popen(['locale', '-a'], stdout=subprocess.PIPE,
+    #                               stderr=subprocess.PIPE).communicate()[0]
+    #     except OSError:
+    #         rv = b''
+    #     good_locales = set()
+    #     has_c_utf8 = False
+    #
+    #     # Make sure we're operating on text here.
+    #     if isinstance(rv, bytes):
+    #         rv = rv.decode('ascii', 'replace')
+    #
+    #     for line in rv.splitlines():
+    #         locale = line.strip()
+    #         if locale.lower().endswith(('.utf-8', '.utf8')):
+    #             good_locales.add(locale)
+    #             if locale.lower() in ('c.utf8', 'c.utf-8'):
+    #                 has_c_utf8 = True
+    #
+    #     extra += '\n\n'
+    #     if not good_locales:
+    #         extra += (
+    #             'Additional information: on this system no suitable UTF-8\n'
+    #             'locales were discovered.  This most likely requires resolving\n'
+    #             'by reconfiguring the locale system.'
+    #         )
+    #     elif has_c_utf8:
+    #         extra += (
+    #             'This system supports the C.UTF-8 locale which is recommended.\n'
+    #             'You might be able to resolve your issue by exporting the\n'
+    #             'following environment variables:\n\n'
+    #             '    export LC_ALL=C.UTF-8\n'
+    #             '    export LANG=C.UTF-8'
+    #         )
+    #     else:
+    #         extra += (
+    #             'This system lists a couple of UTF-8 supporting locales that\n'
+    #             'you can pick from.  The following suitable locales were\n'
+    #             'discovered: %s'
+    #         ) % ', '.join(sorted(good_locales))
+    #
+    #     bad_locale = None
+    #     for locale in os.environ.get('LC_ALL'), os.environ.get('LANG'):
+    #         if locale and locale.lower().endswith(('.utf-8', '.utf8')):
+    #             bad_locale = locale
+    #         if locale is not None:
+    #             break
+    #     if bad_locale is not None:
+    #         extra += (
+    #             '\n\nClick discovered that you exported a UTF-8 locale\n'
+    #             'but the locale system could not pick up from it because\n'
+    #             'it does not exist.  The exported locale is "%s" but it\n'
+    #             'is not supported'
+    #         ) % bad_locale
+    #
+    # raise RuntimeError(
+    #     'Click will abort further execution because Python 3 was'
+    #     ' configured to use ASCII as encoding for the environment.'
+    #     ' Consult https://click.palletsprojects.com/en/7.x/python3/ for'
+    #     ' mitigation steps.' + extra
+    # )
